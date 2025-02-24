690384005f317f07a5e9f5010cd560e84976b2aa
AgathaBahati <matinodev@gmail.com>
2025-02-24 22:44:38 +0300

    feat: define db configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from app.routes import router
+from app.db import init_db
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize the database
+    init_db()
+    yield
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
     app.include_router(router)
     return app
 
---
+++ app/db.py
@@ -0,0 +1,17 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+from .dependencies import get_settings
+# from .models import * 
+
+settings = get_settings()
+
+DATABASE_URL = (
+    f"postgresql://{settings.postgres_user}:{settings.postgres_password}"
+    f"@{settings.db_host}/{settings.postgres_db}"
+)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
